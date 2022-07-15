2f649d7a73b4d11184ec383a9276fc345686d790
Lugo Okeke <lugookeke@icloud.com>
2022-07-15 18:25:35 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -18,6 +18,13 @@ app = Flask(__name__)
 
 
 @app.route("/")
+
+def home_view():
+        return "<h1>Welcome to Geeks for Geeks</h1>"
+
+
+
+
 def hello_world():
     
     chrome_options = webdriver.ChromeOptions()
