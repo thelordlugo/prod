4d68d78f122ce01010011ddf2e3948c5a32a7391
Lugo Okeke <lugookeke@icloud.com>
2022-07-15 18:32:56 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -20,7 +20,8 @@ app = Flask(__name__)
 @app.route("/")
 
 def home_view():
-        return "<h1>Welcome to Geeks for Geeks</h1>"
+        #return "<h1>Welcome to Geeks for Geeks</h1>"
+        return hello_world()
 
 
 
